0dfce038459dc1347c5e92cfaf1fd7e25fdc5c0e
KimJaeUng <78838534+kju2405@users.noreply.github.com>
2023-02-04 22:18:05 +0900

    Singleton Pattern
    - 클래스 인스턴스가 딱 한개만 생성되는 것을 보장하는 디자인 패턴이다.
    - private생성자를 통해서 외부에서 new를 통해 생성하지 못하도록 막는다.
    - 제일 좋은 오류는 컴파일 오류.
    - getInstance()를 통해서만 조회할 수 있따.
    - 싱글톤 패턴의 문제점
    1) 싱글톤 패턴을 구현하는 코드 자체가 많이 들어간다.
    2) private 생성자로 자식 클래스를 만들기 어렵다.
    3) 유연성이 떨어진다.

--- src/test/java/hello/core/singleton/SingletonTest.java
+++ src/test/java/hello/core/singleton/SingletonTest.java
@@ -12,7 +12,7 @@ public class SingletonTest {
 
     @Test
     @DisplayName("스프링 없는 순수한 DI 컨테이너")
-    void pureContainer(){
+    void pureContainer() {
         AppConfig appConfig = new AppConfig();
         MemberService memberService1 = appConfig.memberService();
         MemberService memberService2 = appConfig.memberService();
@@ -21,5 +21,17 @@ public class SingletonTest {
         System.out.println("memberService2 = " + memberService2);
 
         assertThat(memberService1).isNotSameAs(memberService2);
+    }
+ 
+    @Test
+    @DisplayName("싱글톤 패턴을 적용한 객체 사용")
+    void singletonServiceTest(){
+        SingletonService singletonService1 = SingletonService.getInstance();
+        SingletonService singletonService2 = SingletonService.getInstance();
+
+        System.out.println("singletonService1 = " + singletonService1);
+        System.out.println("singletonService2 = " + singletonService2);
+
+        assertThat(singletonService1).isSameAs(singletonService2);
     }
 }
