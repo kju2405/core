fe51462eda3502a8ce87d33fc29d8c9f7610cc39
KimJaeUng <78838534+kju2405@users.noreply.github.com>
2023-02-08 17:03:24 +0900

    Filter
    - includeFilters : 컴포넌트 스캔 대상을 추가로 지정한다.
    - excludeFilters : 컴포넌트 스캔에서 제외할 대상을 지정한다.

--- src/test/java/hello/core/scan/AutoAppConfigTest.java
+++ src/test/java/hello/core/scan/AutoAppConfigTest.java
@@ -17,6 +17,6 @@ public class AutoAppConfigTest {
 
         MemberService memberService = ac.getBean(MemberService.class);
         assertThat(memberService).isInstanceOf(MemberService.class);
-        
+         
     }
 }
