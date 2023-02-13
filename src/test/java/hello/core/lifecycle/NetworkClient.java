20b209f17f8095ac42dcf404c10a0f7ecf7a1d62
KimJaeUng <78838534+kju2405@users.noreply.github.com>
2023-02-13 16:10:13 +0900

    Interface(InitializingBean, DisposableBean)
    - InitializingBean -> afterPropertiesSet() : 초기화콜백
    - DisposableBean -> destroy() : 소멸전 콜백
    - 스프링 전용 인터페이스에 의존한다.
    - 초기화, 소멸 메서드의 이름을 변경할 수 없다.
    -> 요즘은 거의 사용하지 않는다.

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,12 +1,14 @@
 package hello.core.lifecycle;
 
-public class NetworkClient {
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+public class NetworkClient implements InitializingBean, DisposableBean {
 
     private String url;
     public NetworkClient(){
         System.out.println("생성자 호출, url = " + url);
-        connect();
-        call("초기화 연결 메시지");
+
 
     }
 
@@ -26,4 +28,16 @@ public class NetworkClient {
         System.out.println("close: " + url);
     }
 
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("NetworkClient.destroy");
+        disconnect();
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("NetworkClient.afterPropertiesSet");
+        connect();
+        call("초기화 연결 메시지");
+    }
 }
