ff3a43559b6068f6005952bdffc7172222dc780f
KimJaeUng <78838534+kju2405@users.noreply.github.com>
2023-02-15 20:13:02 +0900

    Resolve issues with provider when used with singletonbean
    - 지ㄱ접 필요한 의존관계를 찾는 것을 Dependency Lookup의존관계 조회라고 한다.
    - 지정한 프로토타입 빈을 컨테이너에서 대신 찾아주는 DL정도의 기능만 제공하는 무언가. -> ObjectProvider, JSR-330 Provider

--- src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
+++ src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
@@ -2,12 +2,14 @@ package hello.core.scope;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import javax.inject.Provider;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -33,19 +35,18 @@ public class SingletonWithPrototypeTest1 {
 
 
         ClientBean clientBean2 = ac.getBean(ClientBean.class);
-        assertThat(clientBean2.logic()).isEqualTo(2);
+        assertThat(clientBean2.logic()).isEqualTo(1);
     }
 
     @Scope("singleton")
     static class ClientBean{
-        private final PrototypeBean prototypeBean;
 
         @Autowired
-        public ClientBean(PrototypeBean prototypeBean) {
-            this.prototypeBean = prototypeBean;
-        }
+        private Provider<PrototypeBean> prototypeBeanProvider;
+
 
         public int logic(){
+            PrototypeBean prototypeBean = prototypeBeanProvider.get();
             prototypeBean.addCount();
             int count = prototypeBean.getCount();
             return count;
